9b2259d9e9806d0dfd97faf42ea90daaa14cfa11
yilma <yilma@ERCANMONSTER>
2022-12-28 23:11:32 +0300

    Proje dosyası ekle.

---
+++ DIZILER 3/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DIZILER_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] numbers = { 4, 134, 12 };
+            int total = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                total = total + numbers[i];
+                label1.Text = total.ToString();
+            }
+
+        }
+    }
+}
